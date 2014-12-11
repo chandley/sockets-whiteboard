759be8fb720d2ccff76f9bdbce92d7b3d4018fc4
Richard Lake <ralake1985@gmail.com>
2014-12-11 15:39:31 +0000

    extract jquery into app.js

--- src/socketController.js
+++ src/socketController.js
@@ -1,12 +1,17 @@
 var socket = function(io){
 
-  io.on('connection', function(socket){
-    socket.emit('names', { name: "rich", age: '29' });
-    socket.on('stuff', function(data) {
-      console.log(data.climb)
-    });
-  });
+  // io.on('connection', function(socket){
+  //   socket.emit('names', { name: "rich", age: '29' });
+  //   socket.on('stuff', function(data) {
+  //     console.log(data.climb)
+  //   });
+  // });
 
+	io.on('connection', function(socket) {
+	  socket.on('chat message', function(msg) {
+	  	io.emit('chat message', msg)
+	  });
+	});
 };
 
 module.exports = socket;
