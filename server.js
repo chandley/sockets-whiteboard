265b0c9e52c73dfa19ef1dbb8ccea677f823f93f
Ben Conway <bendavidconway@me.com>
2014-12-11 12:57:56 +0000

    basic websocket setup

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require ('express');
-var app = express(); 
-var server = require('http').createServer(app);
-var io = require('socket.io')(server)
+var app     = express(); 
+var server  = require('http').createServer(app);
+var io      = require('socket.io')(server);
+var socket  = require('./src/socketController')(io);
 
 //view engine setup
-app.use()express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/sockets', function(request, response) {
