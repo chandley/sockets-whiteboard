2cd0ae9b410e363eef85a02470f7c5a0996099ed
Ben Conway <bendavidconway@me.com>
2014-12-11 13:17:39 +0000

    user disconect logic added

--- src/socketController.js
+++ src/socketController.js
@@ -1,8 +1,10 @@
 var socket = function(io){
 
-  io.on('connection', function(){
-
+  io.on('connection', function(socket){
     console.log("Client connected!");
+    socket.on('disconnect', function(){
+      console.log("User Foxtrot Oscar'ed")
+    });
   });
 };
 
