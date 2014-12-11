d33d99d2fe2d555c779a1e438dad8135e011a644
Richard Lake <ralake1985@gmail.com>
2014-12-11 12:23:23 +0000

    Add socketController, sockets.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require ('express');
 var app = express(); 
 var server = require('http').createServer(app);
-var path = require('path');
+var io = require('socket.io')(server)
 
 //view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use()express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+
+app.get('/sockets', function(request, response) {
+  response.render('sockets');
+});
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000");
