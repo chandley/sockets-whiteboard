7005c97cde84bd842c1fe8620f507978426b5692
Richard Lake <ralake1985@gmail.com>
2014-12-12 18:40:06 +0000

    Extract css from html and add extra jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -3,21 +3,24 @@ $(document).ready(function() {
 	var chatname = null;
 
 	$('#username').submit(function(){
+		event.preventDefault();
 		chatname = $('#user').val();
 		socket.emit('username', chatname);
 		$('#user').val('');
-		return false;
+		$(this).fadeOut(500, function() {
+			$('#messagesend').fadeIn(500);	
+		});
 	});
-	
+
 	socket.on('username', function(name){
 		$('#messages').append($('<li>').text(name + ' has entered the room'));
 	});
 
 	$('#messagesend').submit(function(){
+		event.preventDefault();
 		var message = $('#message').val();
 		socket.emit('chat message', { username: chatname, message: message });
 		$('#message').val('');
-		return false;
 	});
 
 	socket.on('chat message', function(msg){
