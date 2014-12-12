9a3e4d2adf01ceaaaa2e5c7d384a6e3fd8213c7f
Richard Lake <ralake1985@gmail.com>
2014-12-12 17:39:46 +0000

    Check jquery cdn links on heroku

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var socket  = require('./src/socketController')(io);
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/sockets', function(request, response) {
+app.get('/', function(request, response) {
   response.render('sockets');
 });
 
-server.listen(3000, function() {
+var port = process.env.PORT || 3000;
+
+server.listen(port, function() {
   console.log("Server listening on port 3000");
 });
 
