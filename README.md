48c7094cbad9451a0cc632f5a2a7f61cd665ea0d
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-15 16:46:33 +0000

    update README

---
+++ README.md
@@ -0,0 +1,2 @@
+Extending the websockets chatroom to a whiteboard
+HTML 5 canvas object allows shared drawing by sending coordinates down websockets connection
--- public/js/canvas.js
+++ public/js/canvas.js
@@ -6,35 +6,33 @@ $(document).ready(function() {
   var draw = false;
   var coOordinates;
 
-$("#whiteboard").mousedown(function (e) {
-  context.moveTo((e.clientX), (e.clientY));
-  socket.emit('sendMove', [e.clientX, e.clientY]) 
-  draw = true;
-});
+  $("#whiteboard").mousedown(function (e) {
+    context.moveTo((e.clientX), (e.clientY));
+    socket.emit('sendMove', [e.clientX, e.clientY]) 
+    draw = true;
+  });
 
-$("#whiteboard").mousemove(function (e) {
-    
-    if(draw) {      
-      socket.emit('sendLine', [e.clientX, e.clientY]) 
-      context.lineTo((e.clientX), (e.clientY));
-      context.stroke();
-    }; 
-                     
-});  
+  $("#whiteboard").mousemove(function (e) {
+      
+      if(draw) {      
+        socket.emit('sendLine', [e.clientX, e.clientY]) 
+        context.lineTo((e.clientX), (e.clientY));
+        context.stroke();
+      }; 
+                       
+  });  
 
-$("#whiteboard").mouseup(function () {
-  draw = false;
-});
+  $("#whiteboard").mouseup(function () {
+    draw = false;
+  });
 
   socket.on('shareLine', function(xypair){
     context.lineTo(xypair[0], xypair[1]);
     context.stroke();
-
   });
 
   socket.on('shareMove', function(xypair){
     context.moveTo(xypair[0], xypair[1]);
-
   });
 
 });
